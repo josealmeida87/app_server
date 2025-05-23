87a75cc2e729912b161b08b6cfa140dc538fd190
drywallcalculadora-finanças <68523382+josealmeida87@users.noreply.github.com>
2025-05-22 21:52:21 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,1 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-import os
 
-def init_firebase():
-    cred_path = os.getenv("FIREBASE_CREDENTIALS")
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(cred_path)
-        firebase_admin.initialize_app(cred)
-    return firestore.client()
