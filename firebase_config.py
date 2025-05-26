ab0c94c150c294d1fee9a4a5fb0184780f2e5c26
drywallcalculadora-finanças <68523382+josealmeida87@users.noreply.github.com>
2025-05-25 23:01:26 -0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,6 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 import os
+load_dotenv()
+
 
 def init_firebase():
     cred_path = os.getenv("FIREBASE_CREDENTIALS")
